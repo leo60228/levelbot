81801ab8b07adce1d8d47c4e6545da6c4c681ef0
leo60228 <iakornfeld@gmail.com>
2020-01-19 19:08:31 -0500

    /levels

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![recursion_limit = "256"]
 
 use async_std::prelude::*;
-use async_std::sync::{channel, Mutex, Sender};
+use async_std::sync::{channel, Mutex, Receiver, Sender};
 use async_std::task::{self, block_on};
 use http::status::StatusCode;
 use serenity::http::AttachmentType;
@@ -16,8 +16,23 @@ pub struct File {
     pub name: String,
 }
 
-pub async fn http_server(sender: Sender<File>) -> Result<'static, ()> {
+#[derive(Clone)]
+pub struct GetLevelsHandle {
+    sender: Sender<()>,
+    receiver: Receiver<Vec<String>>,
+}
+
+pub async fn get_levels(handle: &mut GetLevelsHandle) -> Vec<String> {
+    handle.sender.send(()).await;
+    handle.receiver.next().await.unwrap()
+}
+
+pub async fn http_server(sender: Sender<File>, handle: GetLevelsHandle) -> Result<'static, ()> {
     let mut app = tide::new();
+    app.at("/levels").get(move |_| {
+        let mut handle = handle.clone();
+        async move { get_levels(&mut handle).await.join("\n") }
+    });
     app.at("/upload").post(move |req: Request<()>| {
         let sender = sender.clone();
         let req = Mutex::new(req);
@@ -82,6 +97,12 @@ impl EventHandler for NullHandler {}
 
 fn main() {
     let (http_tx, mut http_rx) = channel(5);
+    let (levels_req_tx, mut levels_req_rx) = channel(5);
+    let (levels_resp_tx, levels_resp_rx) = channel(5);
+    let get_levels_handle = GetLevelsHandle {
+        sender: levels_req_tx,
+        receiver: levels_resp_rx,
+    };
 
     let token = dotenv::var("DISCORD_TOKEN").unwrap();
     let mut client = Client::new(&token, NullHandler).unwrap();
@@ -97,22 +118,60 @@ fn main() {
 
     thread::spawn(move || client.start().unwrap());
 
-    task::spawn(http_server(http_tx));
+    task::spawn(http_server(http_tx, get_levels_handle));
 
     block_on(async move {
+        let mut messages = Vec::new();
         loop {
-            let file = http_rx.next().await.unwrap();
-            channel
-                .read()
-                .send_files(
-                    &http.http,
-                    iter::once(AttachmentType::Bytes {
-                        data: file.data.into(),
-                        filename: file.name,
-                    }),
-                    |msg| msg.content("New level!"),
-                )
-                .unwrap();
+            let send = async {
+                let file = http_rx.next().await.unwrap();
+                channel
+                    .read()
+                    .send_files(
+                        &http.http,
+                        iter::once(AttachmentType::Bytes {
+                            data: file.data.into(),
+                            filename: file.name,
+                        }),
+                        |msg| msg.content("New level!"),
+                    )
+                    .unwrap();
+            };
+            let recv = async {
+                levels_req_rx.next().await.unwrap();
+                let initial = messages.len() == 0;
+                loop {
+                    let received = channel
+                        .read()
+                        .messages(&http.http, |builder| {
+                            if initial {
+                                if let Some(oldest) = messages.last() {
+                                    builder.before(oldest)
+                                } else {
+                                    builder
+                                }
+                            } else {
+                                builder.after(&messages[0])
+                            }
+                            .limit(100)
+                        })
+                        .unwrap();
+                    messages.extend_from_slice(&received);
+                    println!("got {} messages", received.len());
+                    if received.len() < 100 {
+                        break;
+                    }
+                }
+                levels_resp_tx
+                    .send(
+                        messages
+                            .iter()
+                            .filter_map(|x| x.attachments.get(0).map(|x| x.url.clone()))
+                            .collect(),
+                    )
+                    .await;
+            };
+            send.race(recv).await;
         }
     });
 }
